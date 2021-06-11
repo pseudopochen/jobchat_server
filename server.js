74a77f4a0164c1398082cf2ea9e392b17f396122
pseudopochen <pseudopochen@gmail.com>
2021-06-11 16:29:15 -0600

    setup jobchatDB on MongoDB Atlas, connect test

---
+++ server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+mongoose
+  .connect("mongodb+srv://admin:admin@jobchat0.mqguj.mongodb.net/jobchatDB", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log("connected to the database...");
+    app.listen(5000, () => {
+      console.log("server started, listening on port 5000...");
+    });
+  })
+  .catch((e) => {
+    console.log("failed to connect to database", e);
+  });
